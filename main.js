0c8a7c0037a653f221818e1673a3f8dabc5a5640
Shub1970 <18shubham1998@gmail.com>
2024-01-26 22:34:17 +0530

    finaly complete

--- main.js
+++ main.js
@@ -92,3 +92,102 @@ function animateBeforeElement(previousTab, finalActiveTab) {
     { once: true }
   );
 }
+
+// Questions
+
+const questionList = document.querySelectorAll(".Questions li");
+
+function handleInteraction(target) {
+  if (
+    target.tagName === "LI" &&
+    target.getAttribute("aria-selected") === "false"
+  ) {
+    questionList.forEach((tab) => {
+      tab.setAttribute("aria-selected", false);
+      tab.setAttribute("aria-expanded", false);
+    });
+
+    target.setAttribute("aria-selected", true);
+    target.setAttribute("aria-expanded", true);
+  }
+}
+
+function handleMouseOver(event) {
+  handleInteraction(event.target);
+}
+
+function handleKeyDown(event) {
+  if (event.key === "Enter" || event.key === " ") {
+    event.preventDefault();
+    handleInteraction(event.target);
+  } else if (event.key === "ArrowDown" || event.key === "ArrowUp") {
+    event.preventDefault();
+
+    const currentIndex = Array.from(questionList).indexOf(event.target);
+    const nextIndex =
+      event.key === "ArrowDown"
+        ? (currentIndex + 1) % questionList.length
+        : (currentIndex - 1) % questionList.length;
+
+    questionList[nextIndex].focus();
+  }
+}
+
+questionList.forEach((question) => {
+  question.addEventListener("mouseover", handleMouseOver);
+  question.addEventListener("keydown", handleKeyDown);
+});
+
+const connection_form = document.querySelector(".connection-form");
+const submit_btn = connection_form.querySelector("button[type='submit']");
+const input_container = connection_form.querySelector(".input-container");
+
+function isValidEmail(email) {
+  // Regular expression for a basic email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let input_value = connection_form.querySelector("input[type='email']").value;
+
+  if (isValidEmail(input_value)) {
+    input_container.setAttribute("error", false);
+    const formData = new FormData(connection_form);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    connection_form.requestSubmit();
+  } else {
+    input_container.setAttribute("error", true);
+  }
+}
+
+submit_btn.addEventListener("click", handleSubmit);
+
+const nav_button = document.querySelector(".nav-button");
+const all_link = document.querySelector(".all-link");
+const close_button = document.querySelector(".nav-button .close");
+const hamburger_button = document.querySelector(".nav-button .hamburger");
+const primary_company_logo = document.querySelector(".primary-company-logo");
+nav_button.addEventListener("click", function () {
+  let button_aria_expanded = nav_button.getAttribute("aria-expanded");
+  nav_button.setAttribute(
+    "aria-expanded",
+    button_aria_expanded === "true" ? "false" : "true"
+  );
+  [close_button, hamburger_button].forEach((button) => {
+    button.classList.remove("show");
+  });
+  if (nav_button.getAttribute("aria-expanded") === "true") {
+    all_link.classList.add("show");
+    close_button.classList.add("show");
+    nav_button.setAttribute("position", "fixed");
+    primary_company_logo.setAttribute("position", "fixed");
+  } else {
+    all_link.classList.remove("show");
+    hamburger_button.classList.add("show");
+    nav_button.setAttribute("position", "none");
+    primary_company_logo.setAttribute("position", "none");
+  }
+});
